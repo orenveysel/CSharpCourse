9677741d2c783ceede7eb09540e298eec76c8d7a
Veysel ÖREN <orenveysel@outlook.com>
2024-02-29 19:52:14 +0300

    All types

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -12,17 +12,34 @@ namespace TypesAndVariables
         {
             //Value Types
             //Console.WriteLine("Hello World");
+
+            double number5 = 10.4;
+            decimal number6 = 10.4m;
+            char character = 'A';
+            string city = "Ankara";
             bool condition = true;
             byte number4 = 255;   // 8 bit  0 --> 255
             short number3 = -32768;  // 16 bit  -32768 --> 32767
             int number1 = 2147483647;  // 32 bit  -2147483648 --> 2147483647
             long number2 = 9223372036854775807;  // 64 bit  -9223372036854775808 --> 9223372036854775807
+            var number7 = 10;
+            number7 = 'A';
 
             Console.WriteLine("Number1 is {0}",number1);
             Console.WriteLine("Number2 is {0}", number2);
             Console.WriteLine("Number3 is {0}", number3);
             Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number6 is {0}", number6);
+            Console.WriteLine("Number7 is {0}", number7);
+            Console.WriteLine("Character is: {0}", (int)character);
+            Console.WriteLine((int)Days.Friday);
             Console.ReadLine();
         }
+    }
+
+    enum Days
+    {
+        Monday=10, Tuesday=20, Wednesday=30, Trhusday, Friday, Saturday, Sunday
     }
 }
